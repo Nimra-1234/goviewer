709a66569b89760beb889536350954cd296cf0ba
Nimra Tahir <you@example.com>
2023-04-02 14:32:26 +0500

    changes

--- frontend/src/pages/Main/First.jsx
+++ frontend/src/pages/Main/First.jsx
@@ -6,38 +6,26 @@ import MainPage from "./MainPage";
   
 const First = () => {
   return (
-  <div>
+  <div style={{textAlign:"center",backgroundColor:"rgb(53, 51, 54)"}}>
   
     <div>
-      <h1 style={{textAlign:"center"}}><b>Welcome to Apache Age Viewer</b></h1>
+      <h1 style={{textAlign:"center",color:"rgb(255,255,255)"}}><b>Welcome to Apache Age Viewer</b></h1>
     </div>
-
-    <div class="content" style={{
-       display:"block", 
-       width: "50%", 
-       height: "50%", 
-       margin:"auto",
-       padding:"10% 0 0",
-       backgroundPosition:"center",
-       backgroundRepeat:"no-repeat",
-       backgroundSize: "100% 100%",
-       backgroundOrigin: "content-box",
-       animation:"animate 3s linear infinite"}}>
-
-
-      <img src="frontend\src\images\logo.png" id="logo" style={{  
+    <div id="logo">
+       <img src="https://www.apache.org/logos/res/age/age.png"  alt="logo" style={{  
        display: "block",
        width: "50%",
-       height: "50%",
+       height: "50",
        margin: "auto",
        padding: "10% 0 0",
        backgroundPosition:"center",
        backgroundRepeat:"no-repeat",
-       backgroundSize: "100% 100%",backgroundOrigin: "content-box"}} alt="logo"/>
+       backgroundSize: "100% 100%",backgroundOrigin: "content-box"}} />
 
-    </div>
+    </div> 
+   
     <Link to="/second">
-       <button class="popup-button" style={{textAlign:"center",textDecoration: "none",fontWeight: "800",fontSize: "1em",
+       <button class="popup-button" style={{ textAlign:"center",textDecoration: "none",fontWeight: "800",fontSize: "1em",
        textTransform: "uppercase",color: "white", borderRadius:"border-rounded",margin: "10px",padding: "1em 3em",
        backgroundSize: "200% auto",boxShadow:"0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)",
        backgroundImage: "linear-gradient(to right, #540772 0%, #9c0c89 50%, #22129b 100%)",
